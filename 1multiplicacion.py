0923d9dd867ec15f9743a3c028c8201ed0dbe6e6
Katheryn Huaman <61447275@continental.edu.pe>
2026-03-25 21:21:45 -0500

    se cambio los codigos a .py

--- 1multiplicacion.py
+++ 1multiplicacion.py
@@ -1,5 +1,7 @@
+
 numero1 = float(input("Ingresa el primer número: "))
 numero2 = float(input("Ingresa el segundo número: "))
 
 resultado = numero1 * numero2
+
 print("El resultado de la multiplicación es:", resultado)
